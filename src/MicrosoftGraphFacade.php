e8a64c221a2eeecae3c23b54f91f4d6e0b8b81d4
Natpnk <patrick@natpnk.nl>
2024-12-16 22:40:41 +0100

    Documented Facade

--- src/MicrosoftGraphFacade.php
+++ src/MicrosoftGraphFacade.php
@@ -2,8 +2,22 @@
 
 namespace Natpnk\MicrosoftGraphLaravel;
 
-class MicrosoftGraphFacade extends \Illuminate\Support\Facades\Facade {
-    
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Microsoft Graph Facade
+ *
+ * This class provides a facade for the Microsoft Graph service within the Laravel application.
+ */
+class MicrosoftGraphFacade extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * This method defines the service container binding that this facade resolves to.
+     *
+     * @return string The service binding key 'microsoftgraph'.
+     */
     protected static function getFacadeAccessor(){
         return 'microsoftgraph';
     }
